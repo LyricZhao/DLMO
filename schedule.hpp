1ffbcad604f1a3e46cea94e9be8e207d3748d288
LyricZhao <lyricz@yeah.net>
2020-08-19 16:20:04 +0800

    Finish topology analysis

--- schedule.hpp
+++ schedule.hpp
@@ -25,19 +25,28 @@ typedef std::shared_ptr<Schedule> ScheduleHandle;
 // All schedules share a common set of operands
 struct Operand {
     // Can be shared by other schedules
-    size_t size;
+    size_t size = 0;
 
     // Temporary uses
-    TaskHandle prev_gen, prev_use;
+    bool on_device = false, occurred = false;
 
     explicit Operand(size_t size): size(size) {}
+
+    void clear() {
+        on_device = occurred = false;
+    }
+};
+
+struct OperandUsage {
+    OperandHandle operand;
+    TaskHandle gen, prev_use, next_use;
 };
 
 struct Task {
     // Can be shared by other schedules
     std::string name = "none";
     size_t workspace = 0;
-    std::vector<OperandHandle> ins, outs;
+    std::vector<OperandUsage> ins, outs;
     bool hash_calculated = false;
     size_t hash_value = 0;
     uint64_t duration = 0;
@@ -45,7 +54,40 @@ struct Task {
     // Structure
     TaskHandle prev, next;
 
-    // Temporary uses
+    nlohmann::json toJson(std::map<OperandHandle, int> &operand_to_id) {
+        auto usage_to_json = [&operand_to_id](const std::vector<OperandUsage> &usages) {
+            auto json = nlohmann::json::array();
+            for (auto &usage: usages) {
+                json.push_back(operand_to_id[usage.operand]);
+            }
+            return json;
+        };
+        auto json = nlohmann::json::array();
+        json.push_back(name);
+        json.push_back(usage_to_json(ins));
+        json.push_back(usage_to_json(outs));
+        json.push_back(workspace);
+        json.push_back(duration);
+        return json;
+    }
+
+    static TaskHandle dealloc(const std::vector<OperandHandle> &operands) {
+        auto task = std::make_shared<Task>();
+        task->name = ".dealloc";
+        for (auto &operand: operands) {
+            task->outs.push_back(OperandUsage{ operand });
+        }
+        return task;
+    }
+
+    void clear() {
+        for (auto &usage: ins) {
+            usage.prev_use = usage.next_use = usage.gen = nullptr;
+        }
+        for (auto &usage: outs) {
+            usage.prev_use = usage.next_use = usage.gen = nullptr;
+        }
+    }
 
     size_t hash() {
         if (hash_calculated) {
@@ -55,11 +97,11 @@ struct Task {
         hash_value = std::hash<std::string>()(name);
         hash_value = hash_value * 131ull + workspace;
         // Pointer as hash
-        for (auto &operand: ins) {
-            hash_value = hash_value * 131ull + reinterpret_cast<size_t>(operand.get());
+        for (auto &usage: ins) {
+            hash_value = hash_value * 131ull + reinterpret_cast<size_t>(usage.operand.get());
         }
-        for (auto &operand: outs) {
-            hash_value = hash_value * 131ull + reinterpret_cast<size_t>(operand.get());
+        for (auto &usage: outs) {
+            hash_value = hash_value * 131ull + reinterpret_cast<size_t>(usage.operand.get());
         }
         return hash_value;
     }
@@ -78,11 +120,11 @@ struct Task {
 
     static TaskHandle fromJson(const std::vector<OperandHandle> &operands, const nlohmann::json &json) {
         auto task = std::make_shared<Task>();
-        auto fill = [operands](std::vector<OperandHandle> &vec, const nlohmann::json &array) {
+        auto fill = [operands](std::vector<OperandUsage> &vec, const nlohmann::json &array) {
             vec.resize(array.size());
             // Python processor has already assumed `arch == "CUDA"`
             for (int i = 0; i < vec.size(); ++ i) {
-                vec[i] = operands[static_cast<int>(array[i])];
+                vec[i] = OperandUsage{ operands[static_cast<int>(array[i])] };
             }
         };
 
@@ -101,6 +143,171 @@ struct Task {
 
 struct Common {
     std::vector<OperandHandle> operands;
+    std::set<OperandHandle> already_on;
+    std::set<OperandHandle> not_dealloc;
+
+    static CommonHandle fromJson(const nlohmann::json &json) {
+        auto common = std::make_shared<Common>();
+        auto &operands = common->operands;
+        for (auto &item: json) {
+            int id = item[0];
+            size_t size = item[1];
+            if (id >= operands.size()) {
+                operands.resize(id + 1);
+            }
+            operands[id] = std::make_shared<Operand>(size);
+        }
+        return common;
+    }
+
+    void analyze_placement(TaskHandle &head) {
+        // Clear status
+        not_dealloc.clear();
+        already_on.clear();
+        for (auto &operand: operands) {
+            operand->clear();
+        }
+
+        // Analyze `already_on` and `not_dealloc`
+        LOOP(task, head) {
+            if (task->isDealloc()) {
+                for (auto &usage: task->outs) {
+                    usage.operand->on_device = false;
+                }
+            } else {
+                for (auto &usage: task->ins) {
+                    if (not usage.operand->on_device) {
+                        already_on.insert(usage.operand);
+                        usage.operand->on_device = true;
+                    }
+                }
+                for (auto &usage: task->outs) {
+                    usage.operand->on_device = true;
+                }
+            }
+        }
+        for (auto &operand: operands) {
+            if (operand->on_device) {
+                not_dealloc.insert(operand);
+            }
+        }
+    }
+
+    void analyze_topology(TaskHandle &head) const {
+        // Clear status
+        for (auto &operand: operands) {
+            operand->clear();
+        }
+        LOOP(task, head) {
+            task->clear();
+        }
+
+        // Analyze generating task and previous use
+        std::map<OperandHandle, TaskHandle> prev_use;
+        std::map<OperandHandle, TaskHandle> gen;
+        LOOP(task, head) {
+            assert(not task->isDealloc());
+            // TODO: check no duplicate ins/outs
+            for (auto &usage: task->ins) {
+                usage.gen = gen[usage.operand];
+                usage.prev_use = prev_use[usage.operand];
+                prev_use[usage.operand] = task;
+                // TODO: the operation is time consuming
+                // Set the previous' next to current task
+                if (usage.prev_use) {
+                    for (auto &prev_usage: usage.prev_use->ins) {
+                        if (prev_usage.operand == usage.operand) {
+                            prev_usage.next_use = task;
+                            break;
+                        }
+                    }
+                }
+            }
+            for (auto &usage: task->outs) {
+                gen[usage.operand] = task;
+                prev_use[usage.operand] = nullptr;
+            }
+        }
+    }
+
+    static void refactor(TaskHandle &head) {
+        TaskHandle loop_head = head, tail;
+        head = nullptr;
+        LOOP(task, loop_head) {
+            if (not task->isDealloc()) {
+                if (not tail) {
+                    tail = head = task;
+                    task->prev = nullptr;
+                } else {
+                    tail->next = task;
+                    task->prev = tail;
+                    tail = task;
+                }
+            }
+        }
+        tail->next = nullptr;
+    }
+
+    void restore(TaskHandle &head) const {
+        // Analyze topology
+        analyze_topology(head);
+
+        // Insert .dealloc
+        LOOP(task, head) {
+            // Create new .dealloc
+            std::vector<OperandHandle> to_dealloc;
+            for (auto &usage: task->ins) {
+                if (not usage.next_use and not not_dealloc.count(usage.operand)) {
+                    bool inOuts = false;
+                    // TODO: optimize the loop
+                    for (auto &out_usage: task->outs) {
+                        if (usage.operand == out_usage.operand) {
+                            inOuts = true;
+                            break;
+                        }
+                    }
+                    if (not inOuts) {
+                        to_dealloc.push_back(usage.operand);
+                    }
+                }
+            }
+            if (not to_dealloc.empty()) {
+                auto new_task = Task::dealloc(to_dealloc);
+                auto origin_next = task->next;
+                task->next = new_task;
+                new_task->prev = task;
+                new_task->next = origin_next;
+                if (origin_next) {
+                    origin_next->prev = new_task;
+                }
+                task = new_task;
+            }
+        }
+    }
+
+    nlohmann::json toJson(TaskHandle &head) {
+        nlohmann::json json;
+
+        // Push operands
+        json["operands"] = nlohmann::json::array();
+        auto &operands_json = json["operands"];
+        int id = 0;
+        std::map<OperandHandle, int> operand_to_id;
+        for (auto &operand: operands) {
+            operand_to_id[operand] = id;
+            auto operand_json = nlohmann::json::array({id ++, operand->size});
+            operands_json.push_back(operand_json);
+        }
+
+        // Push tasks
+        json["records"] = nlohmann::json::array();
+        auto &records_json = json["records"];
+        LOOP(task, head) {
+            records_json.push_back(task->toJson(operand_to_id));
+        }
+
+        return json;
+    }
 };
 
 struct Schedule {
@@ -140,22 +347,12 @@ struct Schedule {
 
         // Operands
         auto schedule = std::make_shared<Schedule>();
-        auto common = std::make_shared<Common>();
-        auto &operands = common->operands;
-        for (auto &item: json["operands"]) {
-            int id = item[0];
-            size_t size = item[1];
-            if (id >= operands.size()) {
-                operands.resize(id + 1);
-            }
-            operands[id] = std::make_shared<Operand>(size);
-        }
-        schedule->common = common;
+        schedule->common = Common::fromJson(json["operands"]);
 
         // Records
         TaskHandle tail;
         for (auto &item: json["records"]) {
-            auto task = Task::fromJson(operands, item);
+            auto task = Task::fromJson(schedule->common->operands, item);
             if (not tail) {
                 schedule->head = task;
                 tail = task;
@@ -165,7 +362,16 @@ struct Schedule {
                 tail = task;
             }
         }
+        tail->next = nullptr;
 
+        // Analyze common elements
+        schedule->common->analyze_placement(schedule->head);
+        schedule->common->refactor(schedule->head);
+        schedule->common->restore(schedule->head);
+        auto restored_json = schedule->common->toJson(schedule->head);
+        std::ofstream write("dump.json");
+        write << restored_json.dump(4) << std::endl;
+        std::exit(EXIT_SUCCESS);
         return schedule;
     }
 
